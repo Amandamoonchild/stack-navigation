98a4f998364b66b6d99f05b4d1533f44c7a59d1a
Amandamoonchild <amandasilva@aluno.senai.br>
2024-02-26 11:28:31 -0300

    feat: buttpn

---
+++ src/screens/Contact.jsx
@@ -0,0 +1,37 @@
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+
+
+export default function Contact() {
+    const navigation = useNavigation();
+  return (
+    <View>
+        <Text>Contact</Text>
+      <TouchableOpacity
+                style={styles.navButton}
+                onPress={() => navigation.navigate("Home")}
+            >
+                <Text>Home</Text>
+            </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "F5FCFF",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    navButton: {
+        marginTop: 20,
+        padding: 10,
+        backgroundColor: "lightgray",
+        color: "black",
+        borderRadius: 5,
+        width: 60,
+        textAlign: "center",
+    }
+})
